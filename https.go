e4dc42525ac0f4995ef265b3943b8897d6a4088c
thisisaaronland <devnull@localhost>
2020-04-16 13:18:33 -0700

    ensure mux

--- https.go
+++ https.go
@@ -71,5 +71,5 @@ func (s *TLSServer) Address() string {
 }
 
 func (s *TLSServer) ListenAndServe(ctx context.Context, mux *http.ServeMux) error {
-	return http.ListenAndServeTLS(s.url.Host, s.cert, s.key, nil)
+	return http.ListenAndServeTLS(s.url.Host, s.cert, s.key, mux)
 }
