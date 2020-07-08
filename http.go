aae061cb051ca2041b31871d15a93e2ac32810c5
thisisaaronland <devnull@localhost>
2020-07-08 14:37:30 -0700

    first cut at default/configurable timeouts

--- http.go
+++ http.go
@@ -1,10 +1,19 @@
 package server
 
+// https://medium.com/@simonfrey/go-as-in-golang-standard-net-http-config-will-break-your-production-environment-1360871cb72b
+
+// https://ieftimov.com/post/make-resilient-golang-net-http-servers-using-timeouts-deadlines-context-cancellation/
+
+// https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
+// https://blog.cloudflare.com/exposing-go-on-the-internet/
+
 import (
 	"context"
 	_ "log"
 	"net/http"
 	"net/url"
+	"strconv"
+	"time"
 )
 
 func init() {
@@ -14,7 +23,8 @@ func init() {
 
 type HTTPServer struct {
 	Server
-	url *url.URL
+	url         *url.URL
+	http_server *http.Server
 }
 
 func NewHTTPServer(ctx context.Context, uri string) (Server, error) {
@@ -27,8 +37,68 @@ func NewHTTPServer(ctx context.Context, uri string) (Server, error) {
 
 	u.Scheme = "http"
 
+	read_timeout := 1 * time.Second
+	write_timeout := 1 * time.Second
+	idle_timeout := 30 * time.Second
+	header_timeout := 2 * time.Second
+
+	q := u.Query()
+
+	if q.Get("read_timeout") != "" {
+
+		to, err := strconv.Atoi(q.Get("read_timeout"))
+
+		if err != nil {
+			return nil, err
+		}
+
+		read_timeout = time.Duration(to) * time.Second
+	}
+
+	if q.Get("write_timeout") != "" {
+
+		to, err := strconv.Atoi(q.Get("write_timeout"))
+
+		if err != nil {
+			return nil, err
+		}
+
+		write_timeout = time.Duration(to) * time.Second
+	}
+
+	if q.Get("idle_timeout") != "" {
+
+		to, err := strconv.Atoi(q.Get("idle_timeout"))
+
+		if err != nil {
+			return nil, err
+		}
+
+		idle_timeout = time.Duration(to) * time.Second
+	}
+
+	if q.Get("header_timeout") != "" {
+
+		to, err := strconv.Atoi(q.Get("header_timeout"))
+
+		if err != nil {
+			return nil, err
+		}
+
+		header_timeout = time.Duration(to) * time.Second
+	}
+
+	srv := &http.Server{
+		Addr:              u.String(),
+		ReadTimeout:       read_timeout,
+		WriteTimeout:      write_timeout,
+		IdleTimeout:       idle_timeout,
+		ReadHeaderTimeout: header_timeout,
+	}
+
 	server := HTTPServer{
-		url: u,
+		url:         u,
+		http_server: srv,
 	}
 
 	return &server, nil
@@ -39,5 +109,6 @@ func (s *HTTPServer) Address() string {
 }
 
 func (s *HTTPServer) ListenAndServe(ctx context.Context, mux *http.ServeMux) error {
-	return http.ListenAndServe(s.url.Host, mux)
+	return s.ListenAndServe(ctx, mux)
+	// return http.ListenAndServe(s.url.Host, mux)
 }
